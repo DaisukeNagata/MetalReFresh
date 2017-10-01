acf612f74fdc71e91f9069acb70abee533254a78
daisukenagata <dbank0208@gmail.com>
2017-10-01 22:25:30 +0900

    永続化機能の追加

---
+++ MetalReFresh/Classes/ObjectDefaults.swift
@@ -0,0 +1,60 @@
+//
+//  ObjectDefaults.swift
+//  MetalReFresh
+//
+//  Created by nagatadaisuke on 2017/10/01.
+//
+
+import Foundation
+
+public class ObjectDefaults : NSObject {
+    
+    public var userDefaults = UserDefaults.standard
+    public var userArray = Array<Data>()
+    public var imageUserArray :Array<UIImage> = []
+    var imageData = UIImage()
+    
+    public func objectDefaults(index:Int,images:[UIImage])
+    {
+        
+        let dataImages: [Data] = images.map { (image) -> Data in
+            UIImagePNGRepresentation(image)!
+        }
+        
+        userDefaults.set(index, forKey: "index")
+        userDefaults.set(dataImages, forKey: "image")
+        let userImages = userDefaults.object(forKey: "image")
+        
+        userArray += userImages as! Array<Data>
+        
+        for i in 0...userArray.count-1 {
+            
+            imageData = UIImage(data:userArray[i])!
+            imageUserArray.append(imageData)
+            
+        }
+    }
+    
+    public func setObject()->(Array<UIImage>,Int)
+    {
+        let userImages =   ObjectDefaults().userDefaults.object(forKey: "image")
+        let index =   ObjectDefaults().userDefaults.integer(forKey: "index")
+        
+        guard index > 0 else {
+            
+            return (imageUserArray,index)
+            
+        }
+        
+        for i in 0...index-1 {
+            
+            userArray += userImages as! Array<Data>
+            imageData = UIImage(data:userArray[i])!
+            imageUserArray.append(imageData)
+            
+        }
+        
+        return (imageUserArray,index)
+    }
+    
+}
