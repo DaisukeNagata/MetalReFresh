a8b61af0f0f09e5186d9e56f55fd66fd31fbd008
‘daisukenagata; GIT_AUTHOR_EMAIL=‘dbank0208@gmail.com <dbank0208@gmail.com>
2017-12-03 23:57:26 +0900

    add

--- Example/MetalReFresh/ViewController.swift
+++ Example/MetalReFresh/ViewController.swift
@@ -169,26 +169,3 @@ class ViewController: UIViewController,AVCapturePhotoCaptureDelegate,UIImagePick
         
     }
 }
-
-//MARK - Size　Variable
-/*
-extension UIImage {
-    func cropping(to: CGRect) -> UIImage? {
-        var opaque = false
-        if let cgImage = cgImage {
-            switch cgImage.alphaInfo {
-            case .noneSkipLast, .noneSkipFirst:
-                opaque = true
-            default:
-                break
-            }
-        }
-        
-        UIGraphicsBeginImageContextWithOptions(to.size, opaque, scale)
-        draw(at: CGPoint(x: -to.origin.x, y: -to.origin.y))
-        let result = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return result
-    }
-}
-*/
